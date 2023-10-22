40b2d21bfeb2cde88aac3fe4d1f9a69f0d01493a
kadirtanay <ktanay@outlook.com>
2023-10-22 16:22:56 +0300

    memory_game

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const emojis = ["😊", "😊", "🤣", "🤣", "😂", "😂", "🤦‍♂️", "🤦‍♂️", "🤷‍♂️", "🤷‍♂️", "🎶", "🎶", "💖", "💖", "😜", "😜"];
+var shuf_emojis = emojis.sort(() => (Math.random() > .5) ? 2 : -1);
+
+for (var i = 0; i < emojis.length; i++) {
+    let box = document.createElement('div');
+    box.className = 'item';
+    box.innerHTML = shuf_emojis[i]
+
+    box.onclick = function () {
+
+        this.classList.add('boxOpen')
+        setTimeout(function () {
+            if (document.querySelectorAll('.boxOpen').length > 1) {
+
+
+                if (document.querySelectorAll('.boxOpen')[0].innerHTML == document.querySelectorAll('.boxOpen')[1].innerHTML) {
+                    document.querySelectorAll('.boxOpen')[0].classList.add('boxMatch')
+                    document.querySelectorAll('.boxOpen')[1].classList.add('boxMatch')
+
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+
+
+                    if (document.querySelectorAll('.boxMatch').length == emojis.length) {
+                        window.confirm("New")
+                        
+                    }
+
+                } else {
+                    document.querySelectorAll('.boxOpen')[1].classList.remove('boxOpen')
+                    document.querySelectorAll('.boxOpen')[0].classList.remove('boxOpen')
+                }
+            }
+        }, 1000)
+    }
+    document.querySelector('.game').appendChild(box);
+}
